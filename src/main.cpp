6f1a8ea10e6e23db425c266847f1d301afef951e
Dennis Lang <landenlabs2@gmail.com>
2026-05-05 19:50:19 -0400

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-#define VERSION "v6.05.04"
+#define VERSION "v6.05.05"
 
 #include "ll_stdhdr.hpp"
 #include "split.hpp"
